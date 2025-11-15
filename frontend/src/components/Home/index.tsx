03974002bab3bd9ee190fefeb596cc056d81c5c0
DESKTOP-9ICGL95\merge <mihai.mergea@student.upt.ro>
2025-11-15 13:12:05 +0200

    done home

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,10 +1,41 @@
-import { FC } from "react";
+/*
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
 import "./Home.css";
 
-export const Home: FC = () => {
-    return (
-        <div>
-            <p>Hello World!</p>
-        </div>
-    );
+const Home: React.FC = () => {
+  return (
+    <>
+      <main className="homeContainer">
+        <h1 className="homeTitle">Bine ai venit pe pagina noastră</h1>
+
+        <p className="homeSubtitle">Încarcă să ajutăm comunitatea</p>
+
+        <button className="uploadBtn">Încarcă o poză</button>
+      </main>
+    </>
+  );
 };
+
+export default Home;
+*/
+
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="layout">
+      <main className="content">
+        <div className="overlay">
+          <h1>Bine ai venit pe pagina noastră</h1>
+          <p>Încarcă să ajutăm comunitatea</p>
+          <button>Încarcă o poză</button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
